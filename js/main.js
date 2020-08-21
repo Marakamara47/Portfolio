e542daaa9beaca3d74f752e1a686457d54d48905
Marakamara47 <59691437+Marakamara47@users.noreply.github.com>
2020-08-21 15:15:08 +0300

    Mobile test #1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 new WOW().init();
+var mySwiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".project-pagination",
+    type: "bullets",
+    bulletClass: "project-bullet",
+    bulletActiveClass: "project-bullet-active",
+    clickable: true,
+  },
+});
